6ad7ad25d3c5db766f7721f3fda76e91403ee3d1
Phoenix <x.rhythm@qq.com>
2025-07-16 17:19:23 +0800

    feat: add dependencies

---
+++ ReerJSON/Package.swift
@@ -0,0 +1,72 @@
+// swift-tools-version: 5.10
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+#if os(Linux)
+let packageDependencies: [Package.Dependency] = [
+    .package(
+        url: "https://github.com/ibireme/yyjson.git",
+        from: "0.11.1"
+    ),
+]
+#else
+let packageDependencies: [Package.Dependency] = [
+    .package(
+        url: "https://github.com/ibireme/yyjson.git",
+        from: "0.11.1"
+    ),
+    .package(
+        url: "https://github.com/michaeleisel/JJLISO8601DateFormatter.git",
+        from: "0.1.8"
+    ),
+]
+#endif
+
+#if os(Linux)
+let targetDependencies: [Target.Dependency] = [
+    .product(
+        name: "yyjson",
+        package: "yyjson"
+    ),
+]
+#else
+let targetDependencies: [Target.Dependency] = [
+    .product(
+        name: "yyjson",
+        package: "yyjson"
+    ),
+    .product(
+        name: "JJLISO8601DateFormatter",
+        package: "JJLISO8601DateFormatter"
+    ),
+]
+#endif
+
+let package = Package(
+    name: "ReerJSON",
+    platforms: [
+        .macOS(.v10_15),
+        .iOS(.v13),
+        .tvOS(.v13),
+        .watchOS(.v6),
+        .macCatalyst(.v13),
+        .visionOS(.v1)
+    ],
+    products: [
+        .library(
+            name: "ReerJSON",
+            targets: ["ReerJSON"]),
+    ],
+    dependencies: packageDependencies,
+    targets: [
+        .target(
+            name: "ReerJSON",
+            dependencies: targetDependencies
+        ),
+        .testTarget(
+            name: "ReerJSONTests",
+            dependencies: ["ReerJSON"]
+        ),
+    ]
+)
---
+++ ReerJSON/Sources/ReerJSON/ReerJSON.swift
@@ -0,0 +1,5 @@
+// The Swift Programming Language
+// https://docs.swift.org/swift-book
+
+import yyjson
+import JJLISO8601DateFormatter
